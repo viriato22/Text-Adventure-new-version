b7b04ded3076e26bc96af10250018b0fe07da44a
viriato22 <viriato22@gmail.com>
2016-03-13 22:27:03 +0100

    Finished map

--- Text Adventure/Text Adventure/map.cpp
+++ Text Adventure/Text Adventure/map.cpp
@@ -30,12 +30,10 @@ void map::createmap(int n) {
 		id = 2;
 		strcpy_s(name, 25, "South of the clearing");
 		strcpy_s(description, 100, "You see some leaves in the ground put in a well organized fashion.");
-		/*
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		north = 0;
+		south = 8;
+		east = 3;
+		west = 4;
 
 		break;
 	case 3:
@@ -43,12 +41,10 @@ void map::createmap(int n) {
 		id = 3;
 		strcpy_s(name, 25, "East of the clearing");
 		strcpy_s(description, 100, "You see a road that heads east.");
-		/*
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		north = 1;
+		south = 2;
+		east = 9;
+		west = 0;
 
 		break;
 	case 4:
@@ -56,12 +52,10 @@ void map::createmap(int n) {
 		id = 4;
 		strcpy_s(name, 25, "River");
 		strcpy_s(description, 100, "You see a great river flowing from north to south.\nIt seems there is a hut on the other side.");
-		/*
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		north = 1;
+		south = 2;
+		east = 0;
+		west = 7;
 
 		break;
 	case 5:
@@ -69,12 +63,10 @@ void map::createmap(int n) {
 		id = 5;
 		strcpy_s(name, 25, "Destroyed village");
 		strcpy_s(description, 100, "There is a destroyed village in front of you.\n You see small footprints leading west.");
-		/*
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		north = 33;
+		south = 1;
+		east = 33;
+		west = 6;
 
 		break;
 	case 6:
@@ -82,12 +74,10 @@ void map::createmap(int n) {
 		id = 6;
 		strcpy_s(name, 25, "Goblin nest");
 		strcpy_s(description, 100, "You enter a nest where you find some goblins feasting.");
-		/*
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		north = 33;
+		south = 33;
+		east = 5;
+		west = 33;
 
 		break;
 	case 7:
@@ -95,12 +85,10 @@ void map::createmap(int n) {
 		id = 7;
 		strcpy_s(name, 25, "Hut");
 		strcpy_s(description, 100, "There is a hut made of mud and straw, the air around here smells magical.");
-		/*
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		north = 0;
+		south = 33;
+		east = 4;
+		west = 33;
 
 		items[0] = 100;
 
@@ -110,161 +98,137 @@ void map::createmap(int n) {
 		id = 8;
 		strcpy_s(name, 25, "Hole");
 		strcpy_s(description, 100, "It seems that inside that hole was a maze.");
-		/*
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		north = 2;
+		south = 0;
+		east = 33;
+		west = 33;
 
 		break;
 	case 9:
 
-		/*
-		id = ;
-		strcpy_s(name, 25, "");
-		strcpy_s(description, 100, "");
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		id = 9;
+		strcpy_s(name, 25, "Road");
+		strcpy_s(description, 100, "Road leading to the capital");
+		north = 33;
+		south = 10;
+		east = 33;
+		west = 3;
 
 		break;
 	case 10:
 
-		/*
-		id = ;
-		strcpy_s(name, 25, "");
-		strcpy_s(description, 100, "");
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		id = 10;
+		strcpy_s(name, 25, "Gate");
+		strcpy_s(description, 100, "Entrance to the capital");
+		north = 9;
+		south = 14;
+		east = 12;
+		west = 11;
 
 		break;
 	case 11:
 
-		/*
-		id = ;
-		strcpy_s(name, 25, "");
-		strcpy_s(description, 100, "");
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		id = 11;
+		strcpy_s(name, 25, "Armor shop");
+		strcpy_s(description, 100, "Shop that sells armor");
+		north = 33;
+		south = 13;
+		east = 10;
+		west = 33;
 
 		break;
 	case 12:
 
-		/*
-		id = ;
-		strcpy_s(name, 25, "");
-		strcpy_s(description, 100, "");
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		id = 12;
+		strcpy_s(name, 25, "Weapon shop");
+		strcpy_s(description, 100, "Shop that sells armor");
+		north = 33;
+		south = 15;
+		east = 33;
+		west = 10;
 
 		break;
 	case 13:
 
-		/*
-		id = ;
-		strcpy_s(name, 25, "");
-		strcpy_s(description, 100, "");
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		id = 13;
+		strcpy_s(name, 25, "Voodoo woman");
+		strcpy_s(description, 100, "Strange woman that calls herself voodoo woman");
+		north = 11;
+		south = 16;
+		east = 14;
+		west = 33;
 
 		break;
 	case 14:
 
-		/*
-		id = ;
-		strcpy_s(name, 25, "");
-		strcpy_s(description, 100, "");
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		id = 14;
+		strcpy_s(name, 25, "Well");
+		strcpy_s(description, 100, "Ornate well");
+		north = 10;
+		south = 17;
+		east = 15;
+		west = 13;
 
 		break;
 	case 15:
 
-		/*
-		id = ;
-		strcpy_s(name, 25, "");
-		strcpy_s(description, 100, "");
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		id = 15;
+		strcpy_s(name, 25, "Tavern");
+		strcpy_s(description, 100, "Tavern near the docks");
+		north = 12;
+		south = 18;
+		east = 19;
+		west = 14;
 
 		break;
 	case 16:
 
-		/*
-		id = ;
-		strcpy_s(name, 25, "");
-		strcpy_s(description, 100, "");
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		id = 16;
+		strcpy_s(name, 25, "Alley");
+		strcpy_s(description, 100, "Dark alley");
+		north = 13;
+		south = 33;
+		east = 17;
+		west = 33;
 
 		break;
 	case 17:
 
-		/*
-		id = ;
-		strcpy_s(name, 25, "");
-		strcpy_s(description, 100, "");
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		id = 17;
+		strcpy_s(name, 25, "Warehouse");
+		strcpy_s(description, 100, "Warehouse of some big company");
+		north = 14;
+		south = 33;
+		east = 18;
+		west = 16;
 
 		break;
 	case 18:
 
-		/*
-		id = ;
-		strcpy_s(name, 25, "");
-		strcpy_s(description, 100, "");
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		id = 18;
+		strcpy_s(name, 25, "Docks");
+		strcpy_s(description, 100, "Seems like some sort of festival with so many people");
+		north = 15;
+		south = 33;
+		east = 33;
+		west = 17;
 
 		break;
 	case 19:
 
-		/*
-		id = ;
-		strcpy_s(name, 25, "");
-		strcpy_s(description, 100, "");
-		north = ;
-		south = ;
-		east = ;
-		west = ;
-		*/
+		id = 19;
+		strcpy_s(name, 25, "Castle");
+		strcpy_s(description, 100, "Castle fit for a king");
+		north = 33;
+		south = 33;
+		east = 33;
+		west = 15;
 
 		break;
 	}
 
 	/*
-	id = ;
+	id = 20;
 	strcpy_s(name, 25, "");
 	strcpy_s(description, 100, "");
 	north = ;
